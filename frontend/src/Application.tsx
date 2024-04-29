d315a59c17c77f96ad9804cf63a591065b604c03
viola <98323819+voilacf@users.noreply.github.com>
2024-04-29 16:59:50 +0200

    frontend app

---
+++ frontend/src/Application.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Start from "./views/Start";
+import Signin from "./views/Signin";
+import Signup from "./views/Signup";
+import Home from "./views/Home";
+
+function Application() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Start/>}/>
+                <Route path="/signin" element={<Signin/>}/>
+                <Route path="/signup" element={<Signup/>}/>
+                <Route path="/home" element={<Home/>}/>
+            </Routes>
+        </Router>
+    )
+}
+
+export default Application;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Application from './Application.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Application />
+  </React.StrictMode>,
+)
